f5d61845ca209404e5c0e9ad9e025df31fbbf460
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-01-29 14:39:30 +0530

    added db name from monngo vdb colletions

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
 
 const connectDataBase=async()=>{
     try {
         // console.log(process.env.MONOGODB_URI)
-        const connectionDB=await mongoose.connect(`${process.env.MONOGODB_URI}`)
+        const connectionDB=await mongoose.connect(`${process.env.MONOGODB_URI}/${DB_NAME}`)
         // co/
     } catch (error) {
         console.log("error while connecting the database in db/index.js: ",error)
