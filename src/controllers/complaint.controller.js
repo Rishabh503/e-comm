6fa49344ec71cdc94b815e99a13602037182ba60
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-03-07 22:47:58 +0530

    routers of comp

--- src/controllers/complaint.controller.js
+++ src/controllers/complaint.controller.js
@@ -1,5 +1,7 @@
 import { Complaint } from "../models/complaint.model.js";
+import { FollowUp } from "../models/followUp.model.js";
 import { User } from "../models/user.model.js";
+import { Visit } from "../models/visit.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -45,4 +47,69 @@ export const getOneComplaint=asyncHandler(async(req,res)=>{
     if(!complain) throw new ApiError(404,"error extracting the complaint or complaint doesnt exisist");
 
     return res.status(200).json(new ApiResponse(200,complain,"complain sent from backend"))
+})
+
+export const newVisit=asyncHandler(async(req,res)=>{
+    const complaintId=req.params.complaintId;
+    if(!complaintId) throw new ApiError(400,"id not received")
+    const complaint=await Complaint.findById(complaintId).populate("user");
+    if(!complaint) throw new ApiError(400,"id not received")
+        const {
+            visitedBy,
+            dateOfVisit,
+            status,
+            textArea}=req.body
+    
+            if(!visitedBy||!dateOfVisit||!status||! textArea)
+                 throw new ApiError(402,"all fields are required")
+
+                console.log(complaint.user)
+          const newVisit=await Visit.create({
+            name:complaint.companyName,
+            dateOfVisit,
+            visitedBy,
+         problem :complaint.problem,
+         complaintReference:complaint._id,
+        status,
+        location:complaint.user.address,
+        textArea
+            })
+
+    const createdVisit=await Visit.findById(newVisit._id);
+    if(!createdVisit) throw new ApiError(402,"error making visit ")
+
+    console.log(createdVisit)
+
+    complaint.visits=complaint.visits.push(complaint);
+    await complaint.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200,complaint,"visit made succesfully"))
+
+})
+
+export const newFollowUp=asyncHandler(async(req,res)=>{
+    const complaintId=req.params.complaintId;
+    const complaint=await Complaint.findById(complaintId)
+    if(!complaint) throw new ApiError(401,"error finding the complait or complaint doesnt exists")
+    
+    const {name,date,contact,text}=req.body;
+
+    const followUp=await FollowUp.create(
+        {name,
+        date,
+        contact,
+        text,
+        complaintRef:complaint._id
+    }
+    )
+
+    if(!followUp) {
+        throw new Error("")
+        throw new ApiError("402","error creating follow up")}
+    
+    complaint.followUps=complaint.followUps.push(followUp);
+    complaint.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200,followUp,"follow up ban gaya"))
+
 })
--- src/routers/complaint.route.js
+++ src/routers/complaint.route.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { getAllComplaint, getOneComplaint, newComplaint } from "../controllers/complaint.controller.js";
+import { getAllComplaint, getOneComplaint, newComplaint,newVisit,newFollowUp } from "../controllers/complaint.controller.js";
+// import { newFollowUp } from "../controllers/enquiry.controlle.js";
 
 export const complaintRouter=Router();
 complaintRouter.route('/newComplaint/:userId').post(newComplaint)
 complaintRouter.route('/allComplaints').get(getAllComplaint)
 complaintRouter.route('/getComplaint/:complaintId').get(getOneComplaint);
+complaintRouter.route('/:complaintId/newVisit').post(newVisit);
+complaintRouter.route('/:complaintId/followUp').post(newFollowUp);
+
